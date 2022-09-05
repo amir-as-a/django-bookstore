cc03ea200a98a13164ffa34c4cbef759417467da
Amir.s <amir.m.s.137979@gmail.com>
2022-09-05 16:25:05 +0430

    Add User To Book Model How Created The Book

--- books/models.py
+++ books/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 
 
 class Book(models.Model):
+	user = models.ForeignKey(get_user_model(),on_delete = models.CASCADE)
 	title = models.CharField(max_length = 100, blank = True)
 	author = models.CharField(max_length = 100, blank = True)
 	description = models.TextField(blank = True)
